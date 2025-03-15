6d2d77b92b8e7f3d915966fe2ef36d6eecda18c3
Hyper <45052445+HyperBeast43@users.noreply.github.com>
2025-03-14 22:33:38 -0500

    auh

--- card.gd
+++ card.gd
@@ -2,28 +2,20 @@ extends Area2D
 
 var suit:int = 0
 var rank:int = 0
-var faceup:bool = true
+var faceup:bool = false
 var complete:bool = false
-const suitpositions = {
-0 : [Vector2(.5,0)],
-1 : [Vector2(.5,0),Vector2(.5,1)],
-2 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,2)],
-3 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,2),Vector2(.5,3)],
-4 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,2),Vector2(.5,3),Vector2(.5,4)],
-5 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,2),Vector2(.5,3),Vector2(.5,4),Vector2(.5,5)],
-6 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,2),Vector2(.5,3),Vector2(.5,4),Vector2(.5,5),Vector2(.5,6)],
-7 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1)],
-8 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1)],
-9 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1)],
-10 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1)],
-11 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1)],
-12 : [Vector2(.5,0),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1),Vector2(.5,1)]
-}
+var hovered:bool = false
+var dragged:bool = false 
 
+@onready var sprite:Sprite2D = $Sprite2D
 @onready var suitrender:CPUParticles2D = $Sprite2D/suitparticles
 @onready var rankrender:CPUParticles2D = $Sprite2D/rankparticles
 
+
+
 func _ready():
+	sprite.texture = preload("res://sprites/card.png")
+	position = Vector2(30*(rank)+3, 43*(suit+1))
 	while !complete:
 		pass
 	readyup()
@@ -34,8 +26,31 @@ func readyup():
 	suitrender.texture = load("res://sprites/suits/"+str(suit)+".tres")
 	rankrender.texture = load("res://sprites/ranks/"+str(rank)+".tres")
 	var awaaga:PackedVector2Array = suitrender.emission_points
-	for i in range(0,suit):
-		awaaga.append(suitpositions[suit][i])
+	for i in range(0,rank+1):
+		awaaga.append(Vector2(main.suitpositions[rank][i]))
 	suitrender.emission_points = awaaga
-	print(suitrender.emission_points)
-		
+
+func flip():
+	faceup = !faceup
+	suitrender.visible = faceup
+	rankrender.visible = faceup
+	if faceup:
+		sprite.texture = preload("res://sprites/card.png")
+	else:
+		sprite.texture = preload("res://sprites/back.png")
+
+func _process(_delta):
+	var whereami = main.stock.find(self) 
+	if !Input.is_action_pressed("leftclick") and hovered:
+		dragged = false
+	if Input.is_action_just_pressed("leftclick") and hovered and faceup:
+		dragged = true
+	if dragged:
+		position = get_global_mouse_position()-Vector2(13,-20)
+
+
+func _on_mouse_entered() -> void:
+	hovered = true
+
+func _on_mouse_exited() -> void:
+	hovered = false
--- game.gd
+++ game.gd
@@ -1,22 +1,45 @@
 extends Node2D
 
-var pilect:int = 10
-var suitct:int = 8
-var rankct:int = 13
+const suitpositions = {
+0 : [Vector2i(0,0)],
+1 : [Vector2i(0,3),Vector2i(0,-3)],
+2 : [Vector2i(3,3),Vector2i(0,-3),Vector2i(-3,3)],
+3 : [Vector2i(3,3),Vector2i(-3,3),Vector2i(3,-3),Vector2i(-3,-3)],
+4 : [Vector2i(3,6),Vector2i(-3,6),Vector2i(0,0),Vector2i(3,-6),Vector2i(-3,-6)],
+5 : [Vector2i(3,6),Vector2i(-3,6),Vector2i(3,0),Vector2i(-3,0),Vector2i(3,-6),Vector2i(-3,-6)],
+6 : [Vector2i(3,6),Vector2i(-3,6),Vector2i(-6,0),Vector2i(0,0),Vector2i(6,0),Vector2i(3,-6),Vector2i(-3,-6)],
+7 : [Vector2i(6,6),Vector2i(0,6),Vector2i(-6,6),Vector2i(3,0),Vector2i(-3,0),Vector2i(-6,-6),Vector2i(0,-6),Vector2i(6,-6)],
+8 : [Vector2i(6,6),Vector2i(6,0),Vector2i(6,-6),Vector2i(0,6),Vector2i(0,0),Vector2i(0,-6),Vector2i(-6,6),Vector2i(-6,0),Vector2i(-6,-6)],
+9 : [Vector2i(-3,9),Vector2i(3,9),Vector2i(-6,3),Vector2i(0,3),Vector2i(6,3),Vector2i(-6,-3),Vector2i(0,-3),Vector2i(6,-3),Vector2i(-3,-9),Vector2i(3,-9)],
+10 : [Vector2i(-6,9),Vector2i(-6,3),Vector2i(-6,-3),Vector2i(-6,-9),Vector2i(0,6),Vector2i(0,0),Vector2i(0,-6),Vector2i(6,9),Vector2i(6,3),Vector2i(6,-3),Vector2i(6,-9)],
+11 : [Vector2i(-6,9),Vector2i(-6,3),Vector2i(-6,-3),Vector2i(-6,-9),Vector2i(0,9),Vector2i(0,3),Vector2i(0,-3),Vector2i(0,-9),Vector2i(6,9),Vector2i(6,3),Vector2i(6,-3),Vector2i(6,-9)],
+12 : [Vector2i(-6,9),Vector2i(0,9),Vector2i(6,9),Vector2i(-9,-3),Vector2i(0,-3),Vector2i(9,-3),Vector2i(-9,3),Vector2i(-3,3),Vector2i(3,3),Vector2i(9,3),Vector2i(-6,-9),Vector2i(0,-9),Vector2i(6,-9)]
+}
+const pilect:int = 10
+const suitct:int = 8
+const rankct:int = 13
 var piles = []
 var stock = []
 var homes = []
 var stockpile = []
+var cardcount = main.suitct*main.rankct
+var move = null
+var zind:int = 0
 
-func _init():
-	
+func _ready():
+	for i in range(0, pilect):
+		piles.append([])
 	for i in range(0, rankct):
 		for j in range(0,suitct):
 			var card = preload("res://card.tscn").instantiate()
 			card.rank = i
 			card.suit = j
 			card.complete = true
-			card.position = Vector2(30*i+3, 43*j)
 			stock.append(card)
 			add_child(card)
-			
+	stock.shuffle()
+	for i in range(0, pilect):
+		for j in range(0, i+1):
+			piles[i].append(stock.pop_back())
+			piles[i].front().z_index = j
+		piles[i].front().flip()	
